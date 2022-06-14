c540664a46b1e43b3efd680271110b84ae251e98
Saidabror <dasturchisaidabror@gmail.com>
2022-06-14 21:33:53 +0500

    app3

--- Quiz/test.js
+++ Quiz/test.js
@@ -22,4 +22,71 @@ const web = document.querySelector('.web')
 setTimeout(() => {
     loader.style.display = 'none'
     web.style.display = 'block'
-}, 1000);
+}, 3000);
+
+
+
+
+// boshqa
+
+const ff = document.querySelectorAll('#f'),
+    tt = document.querySelectorAll('#t'),
+    qor = document.querySelector('.qorongi'),
+    qor2 = document.querySelector('.qorongi2'),
+    tim = document.querySelector('.time'),
+    tb = document.querySelector('.TBody'),
+    tb2 = document.querySelector('.TBody2'),
+    tb3 = document.querySelector('.TBody3'),
+    tb4 = document.querySelector('.TBody4'),
+    tb5 = document.querySelector('.TBody5'),
+    tb6 = document.querySelector('.TBody6'),
+    tb7 = document.querySelector('.TBody7'),
+    tb8 = document.querySelector('.TBody8'),
+    tb9 = document.querySelector('.TBody9'),
+    tb0 = document.querySelector('.TBody0'),
+    qol = document.querySelector('.qolgani');
+
+var NT = 1
+
+for (let i = 0; i < tt.length; i++) {
+    tt[i].addEventListener('click', function () {
+        qor.style = `
+        display: flex;
+        opacity: 1;
+        `
+        setTimeout(() => {
+            tb2.style.display = 'flex'
+            tb.style.display = 'none'
+            timeLine()
+            NT + 1
+            tim.style.transform = 'translateX(0)'
+            qor.style = `
+            display: none;
+            opacity: 0;
+            `
+        }, 2000);
+    })
+}
+for (let i = 0; i < ff.length; i++) {
+    ff[i].addEventListener('click', function () {
+        qor2.style = `
+        display: flex;
+        opacity: 1;
+        `
+        setTimeout(() => {
+            tb2.style.display = 'flex'
+            tb.style.display = 'none'
+            qor2.style = `
+            display: none;
+            opacity: 0;
+            `
+        }, 2000);
+    })
+}
+
+function timeLine() {
+    tim.style = `
+    transform: translateX(0);
+    `
+}
+qol.innerHTML = `${NT}/10`
--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
 window.addEventListener('DOMContentLoaded', function () {
+    const loader = document.querySelector('.loader')
+    const web = document.querySelector('.web')
+
+    setTimeout(() => {
+        loader.style.display = 'none'
+        web.style.display = 'block'
+    }, 1000);
+
+
     window.addEventListener('scroll', function () {
         const navbar = document.querySelector('.nav');
         navbar.classList.toggle('nav2', window.scrollY > 0)
@@ -15,21 +24,44 @@ window.addEventListener('DOMContentLoaded', function () {
         menuBtn.classList.toggle('active')
         nava.classList.toggle('active')
     })
-})
 
 
-const submi = document.querySelector('.submit_button')
+    const submi = document.querySelector('.submit_button')
+    
+    submi.addEventListener('click', function () {
+        location.href = '#teskt'
+    })
 
-submi.addEventListener('click', function () {
-    location.href = '#teskt'
-})
+    const BtnN1 = document.querySelector('.N2'),
+          BtnN2 = document.querySelector('.N1'),
+          MSec1 = document.querySelector('.mini_section-1'),
+          MSec2 = document.querySelector('.mini_section-2');
 
-window.addEventListener('DOMContentLoaded', () => {
-    const loader = document.querySelector('.loader')
-    const web = document.querySelector('.web')
+    BtnN1.addEventListener('click', function() {
+        MSec2.style.display = 'none'
+        MSec1.style.display = 'block'
+        BtnN1.style = `
+            background: #000;
+            color: #fff;
+        `
+        BtnN2.style = `
+            background: #fff;
+            color: #000;
+        `
 
-    setTimeout(() => {
-        loader.style.display = 'none'
-        web.style.display = 'block'
-    }, 1000);
+    })
+    BtnN2.addEventListener('click', function() {
+        MSec1.style.display = 'none'
+        MSec2.style.display = 'block'
+        BtnN2.style = `
+            background: #000;
+            color: #fff;
+        `
+        BtnN1.style = `
+            background: #fff;
+            color: #000;
+        `
+    })
+
+    
 })
